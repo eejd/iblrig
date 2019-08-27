45e0928b975231cb7a1be89f3aed067be602977d
eejd <edewitt@mac.com>
2019-08-27 12:57:22 +0100

    change task setttings to use c:\iblrig

--- tasks/_iblrig_tasks_biasedChoiceWorldRPE/task_settings.py
+++ tasks/_iblrig_tasks_biasedChoiceWorldRPE/task_settings.py
@@ -2,7 +2,7 @@
 # TASK PARAMETER DEFINITION (should appear on GUI) init trial objects values
 # =============================================================================
 # IBL rig root folder
-IBLRIG_FOLDER = 'C:\\Users\\User\\Documents\\EEJDGitHub\\iblrig'
+IBLRIG_FOLDER = 'C:\\iblrig'
 IBLRIG_DATA_FOLDER = None  # If None data folder will be ..\\iblrig_data from IBLRIG_FOLDER  # noqa
 # SOUND, AMBIENT SENSOR, AND VIDEO RECORDINGS
 RECORD_SOUND = True
